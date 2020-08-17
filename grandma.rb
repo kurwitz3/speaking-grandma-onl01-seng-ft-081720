b4afc303f423c14c45c1d124867e563cdaa60e19
Richard Kurwitz <kurwitz3@aol.com>
2020-08-17 20:19:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(string)
   if string.downcase
     "HUH?! SPEAK UP, SONNY!"
-    elsif string.downcase
+    elsif string.upcase
     "NO NOT SINCE 1938!"    
   elsif string == "I LOVE YOU GRANDMA!"
   "I LOVE YOU TOO PUMPKIN!"
